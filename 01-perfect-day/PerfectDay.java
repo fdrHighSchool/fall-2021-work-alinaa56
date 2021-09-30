d96257cd67b9a8e7f2573d9ec025353de16f37b5
Alina <alina@AC1MWK0JDJC9H.local>
2021-09-30 14:11:05 -0400

    finished week 2 work

---
+++ 01-perfect-day/PerfectDay.java
@@ -0,0 +1,41 @@
+public class PerfectDay {
+
+    public static void intro() {
+      System.out.println("Hello, here is my perfect day!");
+    }
+
+    public static void morning() {
+        System.out.println("Wake up and having a breakfast watching the nature around");
+        System.out.println("Go hiking with my best friend");
+    }
+
+    public static void afternoon() {
+        System.out.println("After that go swimming in the clean lake or river");
+        System.out.println("Come back to house, cook a healthy lunch together and enjoy it");
+    }
+
+    public static void evening() {
+        System.out.println("Then, i would like to spend time on drawing");
+        System.out.println("And watch old horror movie with my friend+eating popcorn!");
+        System.out.println("Before going to sleep - drink a cup of tea,take a bath and talk about this perfect day with my friend");
+    }
+
+    public static void night() {
+      System.out.println("Finally,go to sleep in a warm and soft bed and see spooky dreams");
+    }
+
+    public static void main(String[] args) {
+        intro();
+        morning();
+        afternoon();
+        evening();
+        night();
+
+        System.out.println();
+    }
+}
+
+/*
+newLine("After that go swimming in the clean lake or river");
+newLine("In the evening - watch old horror movies, draw and eating homemade tasty dinner" )
+*/
